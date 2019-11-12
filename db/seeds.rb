a54f0fed81c72cde8af6fc03d8376f59502fd7e3
Juca Esmanhoto <jucaesmanhoto@gmail.com>
2019-11-12 16:50:12 -0300

    seed included

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+30.times do
+  Task.create({
+    title: "#{Faker::Dessert.topping} #{Faker::Creature::Dog.name}",
+    details: Faker::TvShows::MichaelScott.quote})
+end
