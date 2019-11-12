cd36480935c59255129ff35c39bc8e6f112febed
Juca Esmanhoto <jucaesmanhoto@gmail.com>
2019-11-12 16:19:07 -0300

    update task done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get '/tasks/new', to: 'tasks#new'
-  post '/tasks', to: 'tasks#create'
+  get   '/tasks/new',     to: 'tasks#new'
+  post  '/tasks',         to: 'tasks#create'
 
-  get '/tasks', to: 'tasks#index'
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  get   '/tasks',         to: 'tasks#index'
+  get   '/tasks/:id',     to: 'tasks#show', as: 'task'
+
+  get   'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  patch 'tasks/:id',          to: 'tasks#update'
 end
