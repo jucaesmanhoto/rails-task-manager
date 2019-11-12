d01990c06ebdc01c56a1660b6b466fecf4f8d3a1
Juca Esmanhoto <jucaesmanhoto@gmail.com>
2019-11-12 14:01:35 -0300

    create new task, done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'tasks', to: 'tasks#index'
+  get '/tasks/new', to: 'tasks#new'
+  post '/tasks', to: 'tasks#create'
 
-  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  get '/tasks', to: 'tasks#index'
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
 end
